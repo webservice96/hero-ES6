97ce9c10255ceaf4f33e357379c23d29f48ce2a1
webservice96 <dmdhelal@gmail.com>
2021-08-25 14:35:38 +0600

    let & const

---
+++ let-const.js
@@ -0,0 +1,8 @@
+const students = { roll: 120, name: 'Helal Uddin', age: 25, job: 'PHP' };
+students['job'] = 'ES6';
+students.name = 'Kabir Uddin';
+// console.log(students);
+
+let personsData = { name: 'al-amin', phone: '012548966', email: 'dmdhelal23@go.com', country: 'BD' };
+// personsData = { age: 62, userCost: 1520 }
+console.log(personsData);
