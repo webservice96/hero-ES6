699f62a854ede45b6bdfc7de872dca860dce05c1
webservice96 <dmdhelal@gmail.com>
2021-08-25 15:52:17 +0600

    multi line string & dynamic string

---
+++ template-string.js
@@ -0,0 +1,22 @@
+const kobita = 'Amar Bangla';
+const firstLine = "Amar shonar bangla ami tomay....";
+const secondLine = `O ma amr majhe tumi....`;
+
+/* multi line string */
+const multiLine =
+    `This is first line
+    This is second line
+    Thsi is third line
+    `;
+// console.log(multiLine);
+
+
+/* dynamic content */
+const count = 10;
+const freinds = `My freidns is: ${count}`;
+// console.log(freinds);
+
+const firstName = 'Helal';
+const lastName = 'Uddin';
+const fullName = `My full name is: ${firstName} ${lastName}`;
+console.log(fullName);
