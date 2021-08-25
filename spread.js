0c85134d0f9c0679dabf62cde1dccea62d2150e4
webservice96 <dmdhelal@gmail.com>
2021-08-25 17:56:36 +0600

    spread operator

---
+++ spread.js
@@ -0,0 +1,10 @@
+const numbers = [10, 20, 452, 785, 110, 52, 84, 62, 41];
+/* console.log(numbers, '\n', ...numbers); */
+
+const max = Math.max(10, 20, 520, 51);
+
+const maxInarray = Math.max(...numbers);
+/* console.log(maxInarray); */
+
+const newNumbers = [...numbers];
+// console.log(newNumbers);
