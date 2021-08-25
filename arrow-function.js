7fa2aabb381a9bf0a95ed4efc7ed9e01e8bdf057
webservice96 <dmdhelal@gmail.com>
2021-08-25 16:28:47 +0600

    arrow function

---
+++ arrow-function.js
@@ -0,0 +1,17 @@
+/* noramal funciton declaration */
+function add(num1, num2) {
+    return num1 + num2;
+}
+// console.log(add(10, 50));
+
+/* variable anonymus function */
+const multply = function(num1, num2) {
+        return num1 * num2;
+    }
+    // console.log(add2(5, 10));
+
+/* arraow function  */
+const division = (num1, num2) => {
+    return num1 / num2;
+}
+console.log(division(50, 5));
